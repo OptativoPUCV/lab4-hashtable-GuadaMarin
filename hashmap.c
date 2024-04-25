9a0564934208d788add0a4de45f13b6c7b29dd65
GuadaMarin <85418226+GuadaMarin@users.noreply.github.com>
2024-04-25 21:44:01 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -41,7 +41,6 @@ int is_equal(void* key1, void* key2){
 
 void insertMap(HashMap * map, char * key, void * value) {
   
-  if(map==NULL || key==NULL || value==NULL)) return;
   long pos = hash(key, map->capacity);
   while(map->buckets[pos]!=NULL && map->buckets[pos]->key!=NULL){
     if (is_equal(map->buckets[pos]->key, key)) return;
